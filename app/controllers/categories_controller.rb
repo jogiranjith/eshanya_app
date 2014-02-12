cd1bd48cbd6394e2bc37b6c56a8155fbe9cfc287
ranjith <ranjith@cognitiveclouds.com>
2014-02-12 17:01:34 +0530

    added controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,6 @@
 class CategoriesController < ApplicationController
+
+  def category_params
+    params.require(:category).permit(:name, :age)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126114532) do
+ActiveRecord::Schema.define(version: 20140212061256) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140126114532) do
     t.string   "width"
     t.string   "price"
     t.text     "image"
+    t.integer  "category_id"
+    t.integer  "sub_category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -36,13 +38,43 @@ ActiveRecord::Schema.define(version: 20140126114532) do
   create_table "sub_categories", force: true do |t|
     t.string   "name"
     t.text     "image"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "category_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name",                   default: ""
+    t.string   "username",               default: ""
+    t.string   "email",                  default: ""
+    t.string   "about",                  default: ""
+    t.string   "encrypted_password",     default: ""
+    t.string   "city",                   default: ""
+    t.string   "reset_password_token"
+    t.time     "reset_password_sent_at"
+    t.time     "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.time     "current_sign_in_at"
+    t.time     "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "stripe_customer_id"
+    t.boolean  "deleted",                default: false
+    t.string   "confirmation_token"
+    t.time     "confirmed_at"
+    t.time     "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0
+    t.string   "unlock_token"
+    t.time     "locked_at"
+    t.string   "authentication_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "variation_images", force: true do |t|
     t.text     "image"
+    t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
